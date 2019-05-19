a99eefcb747c0e46b4fa3a78eadbc02ad22c3fe3
Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
2019-05-19 09:45:13 +0300

    IB/{core,hw}: ib_pd should not have ib_uobject pointer
    As a preparation step to shared PD, where ib_pd object will be pointed
    by one or more ib_uobjects, remove ib_uobject pointer from ib_pd struct.
    
    Signed-off-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
    Signed-off-by: Shamir Rabinovitch <srabinov7@gmail.com>

--- drivers/infiniband/core/verbs.c
+++ drivers/infiniband/core/verbs.c
@@ -263,7 +263,6 @@ struct ib_pd *__ib_alloc_pd(struct ib_device *device, unsigned int flags,
 		return ERR_PTR(-ENOMEM);
 
 	pd->device = device;
-	pd->uobject = NULL;
 	pd->__internal_mr = NULL;
 	atomic_set(&pd->usecnt, 0);
 	pd->flags = flags;
