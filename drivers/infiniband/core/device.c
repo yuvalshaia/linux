775810975127c72f6c55681187c3f795cdd44a8f
Yuval Shaia <yuval.shaia@oracle.com>
2019-07-01 18:09:13 +0300

    IB/core: Install clone ib_pd in device ops
    Install clone_ib_pd in the device ops.
    
    Signed-off-by: Yuval Shaia <yuval.shaia@oracle.com>

--- drivers/infiniband/core/device.c
+++ drivers/infiniband/core/device.c
@@ -2626,6 +2626,7 @@ void ib_set_device_ops(struct ib_device *dev, const struct ib_device_ops *ops)
 	SET_DEVICE_OP(dev_ops, set_vf_guid);
 	SET_DEVICE_OP(dev_ops, set_vf_link_state);
 	SET_DEVICE_OP(dev_ops, unmap_fmr);
+	SET_DEVICE_OP(dev_ops, clone_ib_pd);
 
 	SET_OBJ_SIZE(dev_ops, ib_ah);
 	SET_OBJ_SIZE(dev_ops, ib_cq);
